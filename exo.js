ce19f7be022cd47dfe5a9ea2171edfacd1118d97
kevinmr59 <kevinmura59610@gmail.com>
2021-11-25 17:04:01 +0100

    math

---
+++ exo.js
@@ -0,0 +1,57 @@
+let one = 34.589456;
+one= Math.floor(one);
+console.log(one);
+
+let two = 68.412784;
+two= Math.floor(two);
+console.log(two);
+
+let three = 39.456;
+three = Math.ceil(three);
+console.log(three);
+
+let four = 27.34876541;
+four = Math.ceil(four);
+console.log(four);
+
+let five = 5.68;
+five = Math.round(five);
+console.log(five);
+
+let six = 5.41;
+six = Math.round(six);
+console.log(six);
+
+let seven = 6.12457;
+seven = Math.trunc(seven);
+console.log(seven);
+
+let height = 9.568124;
+height = Math.trunc(height);
+console.log(height);
+
+let  a = 5, b = 1, c = 12, d = -1, e = 45;
+let minimum =Math.min(a, b, c, d, e);
+console.log(minimum)
+
+
+let  f = 9, g = 12, h = 16, i = 44, j = 3;
+let minimumO =Math.min(f, g, h, i, j);
+console.log(minimumO)
+
+
+let k = 5, l = 1, m = 12, n = -1, o = 45;
+let maximum = Math.max(k, l, m, n, o);
+console.log(maximum);
+
+let  p = 9, q = 12, r = 16, s = 44, t = 3;
+let maximumO =Math.max(p, q, r, s, t);
+console.log(maximumO)
+
+let abs = Math.abs(-6);
+console.log(abs);
+
+let absO = Math.abs("-56");
+console.log(absO);
+
+
